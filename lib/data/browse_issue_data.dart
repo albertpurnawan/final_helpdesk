3b91ed73fc5fadffe5414f91623db246c951693a
ChrisA54 <chrisanggawana@gmail.com>
2022-11-16 00:20:30 +0700

    browse_page

---
+++ lib/data/browse_issue_data.dart
@@ -0,0 +1,30 @@
+import 'package:helpdesk_skripsi/Model/browse_model.dart';
+
+final allIssue = <BrowseIssue>[
+  BrowseIssue(
+      ticketNum: 2201000250006179,
+      createdDate: DateTime.now(),
+      issueDesc: 'Absensi Oka tanggal 14/11/2022 ada 44 orang belum naik.',
+      requestFor: 'MM04996 - Albert',
+      createdBy: 'MM04994 - Daniel',
+      status: 'Open',
+      supportCategory: 'Technical Problem'),
+  BrowseIssue(
+      ticketNum: 2201000250006178,
+      createdDate: DateTime.now(),
+      issueDesc:
+          'Mohon bantuan permintaan zoom meeting : Nama : Cornelia Natasha Febby NIK : 00092967 Department / Divisi : HR Corporate / Recruiter Tanggal : Rabu, 16 November 2022 Waktu : 15.00. s/d 16.30 WIB Tema / Topik : Interview User Financial Reporting PT Mayora Indah Tbk',
+      requestFor: 'MM04994 - Daniel',
+      createdBy: 'MM04996 - Chris Anggawana',
+      status: 'Close',
+      supportCategory: 'Network Problem'),
+  BrowseIssue(
+      ticketNum: 2201000250006177,
+      createdDate: DateTime.now(),
+      issueDesc:
+          'Mohon bantuannya untuk kebutuhan link zoom meeting dbi: Nama : Aluisius Dwiki NIK : 100717 Department / Divisi : Sisdur Hari & Tanggal : Kamis, 17 Nov 2022 Waktu : 13.30 - Selesai (Waktu mohon agar Tentative sampe Sore karena mempertimbangkan adanya perubahan Waktu) Tema & Topik : Meeting Review Pencapaian KPI Operational 2022 - Import & Accounting GL',
+      requestFor: 'MM04991 - Albert',
+      createdBy: 'MM04994 - Daniel',
+      status: 'Review',
+      supportCategory: 'Application Problem')
+];
--- lib/pages/browse_page.dart
+++ lib/pages/browse_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:helpdesk_skripsi/Model/browse_model.dart';
+import 'package:helpdesk_skripsi/data/browse_issue_data.dart';
 import 'package:helpdesk_skripsi/style.dart';
 import 'package:helpdesk_skripsi/util/appbar.dart';
 import 'package:helpdesk_skripsi/util/bottom_navbar.dart';
+import 'package:intl/intl.dart';
 
 class BrowsePage extends StatefulWidget {
   const BrowsePage({super.key});
@@ -12,305 +15,178 @@ class BrowsePage extends StatefulWidget {
 }
 
 class _BrowsePageState extends State<BrowsePage> {
-  final DataTableSource _dataIssue = MyData();
+  List<BrowseIssue>? issueData;
+
+  @override
+  void initState() {
+    issueData = allIssue;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const MyAppBar(),
       backgroundColor: primaryColor,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Browse List Issue",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: secondaryColor,
-                ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: ListView.builder(
+                itemCount: allIssue.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 10,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 20, bottom: 20, top: 30),
+                              child: SizedBox(
+                                width: 130,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Ticket Number",
+                                      style: GoogleFonts.inter(
+                                          fontSize: 15,
+                                          color: secondaryColor,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      allIssue[index].ticketNum.toString(),
+                                      style: GoogleFonts.inter(
+                                        fontSize: 12,
+                                        color: secondaryColor,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Request For",
+                                      style: GoogleFonts.inter(
+                                          fontSize: 15,
+                                          color: secondaryColor,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      allIssue[index].requestFor.toString(),
+                                      style: GoogleFonts.inter(
+                                        fontSize: 12,
+                                        color: secondaryColor,
+                                      ),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    Text(
+                                      "Status",
+                                      style: GoogleFonts.inter(
+                                          fontSize: 15,
+                                          color: secondaryColor,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      allIssue[index].status.toString(),
+                                      style: GoogleFonts.inter(
+                                        fontSize: 12,
+                                        color: secondaryColor,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 140,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Created Date",
+                                    style: GoogleFonts.inter(
+                                        fontSize: 15,
+                                        color: secondaryColor,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    DateFormat.yMd()
+                                        .format(allIssue[index].createdDate),
+                                    style: GoogleFonts.inter(
+                                      fontSize: 12,
+                                      color: secondaryColor,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Created By",
+                                    style: GoogleFonts.inter(
+                                        fontSize: 15,
+                                        color: secondaryColor,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    allIssue[index].createdBy.toString(),
+                                    style: GoogleFonts.inter(
+                                      fontSize: 12,
+                                      color: secondaryColor,
+                                    ),
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  Text(
+                                    "Support Category",
+                                    style: GoogleFonts.inter(
+                                        fontSize: 15,
+                                        color: secondaryColor,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    allIssue[index].supportCategory.toString(),
+                                    style: GoogleFonts.inter(
+                                      fontSize: 12,
+                                      color: secondaryColor,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(
+                              left: 20, right: 20, bottom: 5),
+                          child: Text(
+                            "Issue Desc",
+                            style: GoogleFonts.inter(
+                                fontSize: 15,
+                                color: secondaryColor,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(left: 20, right: 20),
+                          child: Text(
+                            allIssue[index].issueDesc.toString(),
+                            style: GoogleFonts.inter(
+                              fontSize: 12,
+                              color: secondaryColor,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              const SizedBox(
-                width: 200,
-                height: 30,
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.search),
-                    border: OutlineInputBorder(),
-                    hintText: "Search...",
-                    contentPadding: EdgeInsets.all(5),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              PaginatedDataTable(
-                columns: _createColumns(),
-                source: _dataIssue,
-                columnSpacing: 40,
-                horizontalMargin: 20,
-                rowsPerPage: 5,
-                arrowHeadColor: secondaryColor,
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
       bottomNavigationBar: const BottomNavbar(),
       // drawer: const MyDrawer(),
     );
   }
-}
-
-List<DataColumn> _createColumns() {
-  return [
-    DataColumn(
-        label: Text(
-      'Actions',
-      style: GoogleFonts.inter(),
-    )),
-    DataColumn(
-        label: Text(
-      'Ticket Number',
-      style: GoogleFonts.inter(),
-    )),
-    DataColumn(
-        label: Text(
-      'Created Date',
-      style: GoogleFonts.inter(),
-    )),
-    DataColumn(
-        label: Text(
-      'Issue Desc',
-      style: GoogleFonts.inter(),
-    )),
-    DataColumn(
-        label: Text(
-      'Request For',
-      style: GoogleFonts.inter(),
-    )),
-    DataColumn(
-        label: Text(
-      'Created By',
-      style: GoogleFonts.inter(),
-    )),
-    DataColumn(
-        label: Text(
-      'Status',
-      style: GoogleFonts.inter(),
-    )),
-    DataColumn(
-        label: Text(
-      'Support Category',
-      style: GoogleFonts.inter(),
-    ))
-  ];
-}
-
-class MyData extends DataTableSource {
-  final List<Map<String, dynamic>> _dataIssue = [
-    {
-      'Ticket_Num': 102,
-      'CreatedDate': '01-11-2022',
-      'Issue': 'Gak bisa buka',
-      'RequestFor': 'MM04996 - Chris Anggawana',
-      'CreatedBy': 'MM04994 - Daniel',
-      'Status': 'Open',
-      'SupportCat': 'Technical Problem'
-    },
-    {
-      'Ticket_Num': 101,
-      'CreatedDate': '01-11-2022',
-      'Issue': 'Gak bisa buka',
-      'RequestFor': 'MM04996 - Chris Anggawana',
-      'CreatedBy': 'MM04994 - Daniel',
-      'Status': 'Open',
-      'SupportCat': 'Technical Problem'
-    },
-    {
-      'Ticket_Num': 100,
-      'CreatedDate': '01-11-2022',
-      'Issue': 'Gak bisa buka',
-      'RequestFor': 'MM04996 - Chris Anggawana',
-      'CreatedBy': 'MM04994 - Daniel',
-      'Status': 'Open',
-      'SupportCat': 'Technical Problem'
-    },
-    {
-      'Ticket_Num': 103,
-      'CreatedDate': '01-11-2022',
-      'Issue': 'Gak bisa buka',
-      'RequestFor': 'MM04996-Chris Anggawana',
-      'CreatedBy': 'MM04994 - Daniel',
-      'Status': 'Open',
-      'SupportCat': 'Technical Problem'
-    },
-    {
-      'Ticket_Num': 104,
-      'CreatedDate': '01-11-2022',
-      'Issue': 'Gak bisa buka',
-      'RequestFor': 'MM04996-Chris Anggawana',
-      'CreatedBy': 'MM04994 - Daniel',
-      'Status': 'Open',
-      'SupportCat': 'Technical Problem'
-    },
-    {
-      'Ticket_Num': 105,
-      'CreatedDate': '01-11-2022',
-      'Issue': 'Gak bisa buka',
-      'RequestFor': 'MM04996-Chris Anggawana',
-      'CreatedBy': 'MM04994 - Daniel',
-      'Status': 'Open',
-      'SupportCat': 'Technical Problem'
-    },
-    {
-      'Ticket_Num': 106,
-      'CreatedDate': '01-11-2022',
-      'Issue': 'Gak bisa buka',
-      'RequestFor': 'MM04996-Chris Anggawana',
-      'CreatedBy': 'MM04994 - Daniel',
-      'Status': 'Open',
-      'SupportCat': 'Technical Problem'
-    },
-    {
-      'Ticket_Num': 107,
-      'CreatedDate': '01-11-2022',
-      'Issue': 'Gak bisa buka',
-      'RequestFor': 'MM04996-Chris Anggawana',
-      'CreatedBy': 'MM04994 - Daniel',
-      'Status': 'Open',
-      'SupportCat': 'Technical Problem'
-    },
-    {
-      'Ticket_Num': 108,
-      'CreatedDate': '01-11-2022',
-      'Issue': 'Gak bisa buka',
-      'RequestFor': 'MM04996-Chris Anggawana',
-      'CreatedBy': 'MM04994 - Daniel',
-      'Status': 'Open',
-      'SupportCat': 'Technical Problem'
-    },
-    {
-      'Ticket_Num': 109,
-      'CreatedDate': '01-11-2022',
-      'Issue': 'Gak bisa buka',
-      'RequestFor': 'MM04996-Chris Anggawana',
-      'CreatedBy': 'MM04994 - Daniel',
-      'Status': 'Open',
-      'SupportCat': 'Technical Problem'
-    },
-    {
-      'Ticket_Num': 110,
-      'CreatedDate': '01-11-2022',
-      'Issue': 'Gak bisa buka',
-      'RequestFor': 'MM04996-Chris Anggawana',
-      'CreatedBy': 'MM04994 - Daniel',
-      'Status': 'Open',
-      'SupportCat': 'Technical Problem'
-    },
-    {
-      'Ticket_Num': 111,
-      'CreatedDate': '01-11-2022',
-      'Issue': 'Gak bisa buka',
-      'RequestFor': 'MM04996-Chris Anggawana',
-      'CreatedBy': 'MM04994 - Daniel',
-      'Status': 'Open',
-      'SupportCat': 'Technical Problem'
-    },
-    {
-      'Ticket_Num': 112,
-      'CreatedDate': '01-11-2022',
-      'Issue': 'Gak bisa buka',
-      'RequestFor': 'MM04996-Chris Anggawana',
-      'CreatedBy': 'MM04994 - Daniel',
-      'Status': 'Open',
-      'SupportCat': 'Technical Problem'
-    },
-    {
-      'Ticket_Num': 113,
-      'CreatedDate': '01-11-2022',
-      'Issue': 'Gak bisa buka',
-      'RequestFor': 'MM04996-Chris Anggawana',
-      'CreatedBy': 'MM04994 - Daniel',
-      'Status': 'Open',
-      'SupportCat': 'Technical Problem'
-    },
-    {
-      'Ticket_Num': 114,
-      'CreatedDate': '01-11-2022',
-      'Issue': 'Gak bisa buka',
-      'RequestFor': 'MM04996-Chris Anggawana',
-      'CreatedBy': 'MM04994 - Daniel',
-      'Status': 'Open',
-      'SupportCat': 'Technical Problem'
-    },
-    {
-      'Ticket_Num': 115,
-      'CreatedDate': '01-11-2022',
-      'Issue': 'Gak bisa buka',
-      'RequestFor': 'MM04996-Chris Anggawana',
-      'CreatedBy': 'MM04994 - Daniel',
-      'Status': 'Open',
-      'SupportCat': 'Technical Problem'
-    }
-  ];
-
-  @override
-  DataRow? getRow(int index) {
-    return DataRow(cells: [
-      DataCell(Row(
-        children: [
-          IconButton(
-            icon: const Icon(Icons.density_small_rounded, size: 20),
-            onPressed: () {
-              print('delete clicked');
-            },
-          ),
-        ],
-      )),
-      DataCell(Text(
-        _dataIssue[index]['Ticket_Num'].toString(),
-        style: GoogleFonts.inter(),
-      )),
-      DataCell(Text(
-        _dataIssue[index]['CreatedDate'],
-        style: GoogleFonts.inter(),
-      )),
-      DataCell(Text(
-        _dataIssue[index]['Issue'],
-        style: GoogleFonts.inter(),
-      )),
-      DataCell(Text(
-        _dataIssue[index]['RequestFor'],
-        style: GoogleFonts.inter(),
-      )),
-      DataCell(Text(
-        _dataIssue[index]['CreatedBy'],
-        style: GoogleFonts.inter(),
-      )),
-      DataCell(Text(
-        _dataIssue[index]['Status'],
-        style: GoogleFonts.inter(),
-      )),
-      DataCell(Text(
-        _dataIssue[index]['SupportCat'],
-        style: GoogleFonts.inter(),
-      ))
-    ]);
-  }
-
-  @override
-  bool get isRowCountApproximate => false;
-
-  @override
-  int get rowCount => _dataIssue.length;
-
-  @override
-  int get selectedRowCount => 0;
 }
