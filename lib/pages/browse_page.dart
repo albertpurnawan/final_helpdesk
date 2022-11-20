3af3a82f3e0f7ebb5a6b6957630d083c92e70995
ChrisA54 <chrisanggawana@gmail.com>
2022-11-20 22:14:21 +0700

    Search Halaman Browse

--- lib/pages/browse_page.dart
+++ lib/pages/browse_page.dart
@@ -5,6 +5,7 @@ import 'package:helpdesk_skripsi/data/browse_issue_data.dart';
 import 'package:helpdesk_skripsi/style.dart';
 import 'package:helpdesk_skripsi/util/appbar.dart';
 import 'package:helpdesk_skripsi/util/bottom_navbar.dart';
+import 'package:helpdesk_skripsi/util/searchBar.dart';
 import 'package:intl/intl.dart';
 
 class BrowsePage extends StatefulWidget {
@@ -15,13 +16,8 @@ class BrowsePage extends StatefulWidget {
 }
 
 class _BrowsePageState extends State<BrowsePage> {
-  List<BrowseIssue>? issueData;
-
-  @override
-  void initState() {
-    issueData = allIssue;
-    super.initState();
-  }
+  List<BrowseIssue> issueData = allIssue;
+  String query = '';
 
   @override
   Widget build(BuildContext context) {
@@ -30,171 +26,17 @@ class _BrowsePageState extends State<BrowsePage> {
       backgroundColor: primaryColor,
       body: Column(
         children: [
+          buildSearch(),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(20),
               child: ListView.builder(
-                itemCount: allIssue.length,
+                itemCount: issueData.length,
                 itemBuilder: (context, index) {
-                  return Card(
-                    elevation: 10,
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(20),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              // COLUMN KIRI
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Ticket Number",
-                                    style: GoogleFonts.inter(
-                                        fontSize: 15,
-                                        color: secondaryColor,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(5),
-                                    child: Text(
-                                      allIssue[index].ticketNum.toString(),
-                                      style: GoogleFonts.inter(
-                                        fontSize: 12,
-                                        color: secondaryColor,
-                                      ),
-                                    ),
-                                  ),
-                                  Text(
-                                    "Request For",
-                                    style: GoogleFonts.inter(
-                                        fontSize: 15,
-                                        color: secondaryColor,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(5),
-                                    child: SizedBox(
-                                      width: 130,
-                                      child: Text(
-                                        allIssue[index].requestFor.toString(),
-                                        style: GoogleFonts.inter(
-                                          fontSize: 12,
-                                          color: secondaryColor,
-                                        ),
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                    ),
-                                  ),
-                                  Text(
-                                    "Status",
-                                    style: GoogleFonts.inter(
-                                        fontSize: 15,
-                                        color: secondaryColor,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(5),
-                                    child: Text(
-                                      allIssue[index].status.toString(),
-                                      style: GoogleFonts.inter(
-                                        fontSize: 12,
-                                        color: secondaryColor,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              // COLUMN KANAN
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Created Date",
-                                    style: GoogleFonts.inter(
-                                        fontSize: 15,
-                                        color: secondaryColor,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(5),
-                                    child: Text(
-                                      DateFormat.yMd()
-                                          .format(allIssue[index].createdDate),
-                                      style: GoogleFonts.inter(
-                                        fontSize: 12,
-                                        color: secondaryColor,
-                                      ),
-                                    ),
-                                  ),
-                                  Text(
-                                    "Created By",
-                                    style: GoogleFonts.inter(
-                                        fontSize: 15,
-                                        color: secondaryColor,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(5),
-                                    child: SizedBox(
-                                      width: 130,
-                                      child: Text(
-                                        allIssue[index].createdBy.toString(),
-                                        style: GoogleFonts.inter(
-                                          fontSize: 12,
-                                          color: secondaryColor,
-                                        ),
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                    ),
-                                  ),
-                                  Text(
-                                    "Support Category",
-                                    style: GoogleFonts.inter(
-                                        fontSize: 15,
-                                        color: secondaryColor,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(5),
-                                    child: Text(
-                                      allIssue[index]
-                                          .supportCategory
-                                          .toString(),
-                                      style: GoogleFonts.inter(
-                                        fontSize: 12,
-                                        color: secondaryColor,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                        Text(
-                          "Issue Desc",
-                          style: GoogleFonts.inter(
-                              fontSize: 15,
-                              color: secondaryColor,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.only(
-                              left: 20, right: 20, bottom: 20),
-                          child: Text(
-                            allIssue[index].issueDesc.toString(),
-                            style: GoogleFonts.inter(
-                              fontSize: 12,
-                              color: secondaryColor,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ],
-                    ),
+                  final issue = issueData[index];
+                  return GestureDetector(
+                    child: buildCard(issue),
+                    onDoubleTap: () => Navigator.push(context, ),
                   );
                 },
               ),
@@ -205,5 +47,190 @@ class _BrowsePageState extends State<BrowsePage> {
       bottomNavigationBar: const BottomNavbar(),
       // drawer: const MyDrawer(),
     );
+  }
+
+  Card buildCard(BrowseIssue issue) {
+    return Card(
+      elevation: 10,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // COLUMN KIRI
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Ticket Number",
+                      style: GoogleFonts.inter(
+                          fontSize: 15,
+                          color: secondaryColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Text(
+                        issue.ticketNum.toString(),
+                        style: GoogleFonts.inter(
+                          fontSize: 12,
+                          color: secondaryColor,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Request For",
+                      style: GoogleFonts.inter(
+                          fontSize: 15,
+                          color: secondaryColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: SizedBox(
+                        width: 130,
+                        child: Text(
+                          issue.requestFor.toString(),
+                          style: GoogleFonts.inter(
+                            fontSize: 12,
+                            color: secondaryColor,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Status",
+                      style: GoogleFonts.inter(
+                          fontSize: 15,
+                          color: secondaryColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Text(
+                        issue.status.toString(),
+                        style: GoogleFonts.inter(
+                          fontSize: 12,
+                          color: secondaryColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                // COLUMN KANAN
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Created Date",
+                      style: GoogleFonts.inter(
+                          fontSize: 15,
+                          color: secondaryColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Text(
+                        DateFormat.yMd().format(issue.createdDate),
+                        style: GoogleFonts.inter(
+                          fontSize: 12,
+                          color: secondaryColor,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Created By",
+                      style: GoogleFonts.inter(
+                          fontSize: 15,
+                          color: secondaryColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: SizedBox(
+                        width: 130,
+                        child: Text(
+                          issue.createdBy.toString(),
+                          style: GoogleFonts.inter(
+                            fontSize: 12,
+                            color: secondaryColor,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Support Category",
+                      style: GoogleFonts.inter(
+                          fontSize: 15,
+                          color: secondaryColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Text(
+                        issue.supportCategory.toString(),
+                        style: GoogleFonts.inter(
+                          fontSize: 12,
+                          color: secondaryColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Text(
+            "Issue Desc",
+            style: GoogleFonts.inter(
+                fontSize: 15,
+                color: secondaryColor,
+                fontWeight: FontWeight.bold),
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+            child: Text(
+              issue.issueDesc.toString(),
+              style: GoogleFonts.inter(
+                fontSize: 12,
+                color: secondaryColor,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildSearch() => SearchWidget(
+        text: query,
+        hintText: 'Search...',
+        onChanged: searchIssue,
+      );
+
+  void searchIssue(String query) {
+    final tickets = allIssue.where((issue) {
+      final ticketNum = issue.ticketNum.toString().toLowerCase();
+      final createdBy = issue.createdBy.toLowerCase();
+      final requestFor = issue.requestFor.toLowerCase();
+      final suupportCategory = issue.supportCategory.toLowerCase();
+      final searchLower = query.toLowerCase();
+
+      return ticketNum.contains(searchLower) ||
+          createdBy.contains(searchLower) ||
+          requestFor.contains(searchLower) ||
+          suupportCategory.contains(searchLower);
+    }).toList();
+
+    setState(() {
+      this.query = query;
+      issueData = tickets;
+    });
   }
 }
---
+++ lib/util/searchBar.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:helpdesk_skripsi/style.dart';
+
+class SearchWidget extends StatefulWidget {
+  final String text;
+  final ValueChanged<String> onChanged;
+  final String hintText;
+
+  const SearchWidget({
+    Key? key,
+    required this.text,
+    required this.onChanged,
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  _SearchWidgetState createState() => _SearchWidgetState();
+}
+
+class _SearchWidgetState extends State<SearchWidget> {
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 42,
+      margin: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.white,
+      ),
+      child: TextField(
+        controller: controller,
+        style: GoogleFonts.inter(color: secondaryColor),
+        decoration: InputDecoration(
+          prefixIcon: const Icon(Icons.search),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(
+              color: secondaryColor,
+              width: 1.0,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(
+              color: secondaryColor,
+              width: 1.0,
+            ),
+          ),
+          hintText: widget.hintText,
+          hintStyle: GoogleFonts.inter(
+            color: secondaryColor,
+            fontSize: 14,
+          ),
+          border: InputBorder.none,
+        ),
+        onChanged: widget.onChanged,
+      ),
+    );
+  }
+}
